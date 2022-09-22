f6ef3200587ba19ed5826985c00fecb039ec0a31
Sneha Hegde <snehahegde33@gmail.com>
2022-09-22 08:36:36 +0530

    dynamic expenses

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import Expenses from './components/Expenses/Expenses';
 
 import NewExpense from './components/NewExpense/NewExpense';
-const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
 
+import { useState } from 'react';
+
+const INITIAL_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+const App = () => {
   //expenseData is lifted up from the NewExpense to the App
   //since Expenses needs this data, we need it to be lifted up to App component
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+
   const addExpenseHandler = (expense) => {
-    console.log('In app.js');
-    console.log(expense);
+    //prevExpense scince its based on older snapshot
+    setExpenses((prevExpenses) => {
+      return [expense, ...expenses];
+    });
   };
 
   return (
